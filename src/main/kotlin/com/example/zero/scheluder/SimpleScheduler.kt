76052a8d59c84388d48314b7a03fe3ecf8235f73
Vlad <104162046+LaMbUsErNaMe@users.noreply.github.com>
2025-12-23 16:56:23 +0400

    sheluders, aspects, log to file

---
+++ src/main/kotlin/com/example/zero/scheluder/SimpleScheduler.kt
@@ -0,0 +1,17 @@
+package com.example.zero.scheluder
+
+import com.example.zero.services.ProductService
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+
+@Component
+@ConditionalOnProperty(prefix = "app", name = ["schedule.optimization"], havingValue = "false")
+class SimpleScheduler (
+    private val productService: ProductService
+){
+    @Scheduled(fixedDelayString = $$"${app.schedule.period}")
+    fun priceUpScheduler() {
+        productService.priceUp()
+    }
+}
