3d38a98e46ac4f9cf558d60bf118c53286786043
Silly <104162046+LaMbUsErNaMe@users.noreply.github.com>
2026-01-15 08:03:50 +0400

    fix

--- src/main/kotlin/com/example/zero/search/ProductField.kt
+++ src/main/kotlin/com/example/zero/search/ProductField.kt
@@ -1,39 +1,48 @@
 package com.example.zero.search
 
 import com.example.zero.enums.CategoryType
-import com.example.zero.enums.OperationType
 import java.math.BigDecimal
+import java.time.LocalDate
+import java.time.LocalDateTime
 
 enum class ProductField(
     val path: String,
     val type: Class<*>,
-    val allowedOperations: Set<OperationType>,
     val enumClass: Class<out Enum<*>>? = null
 ) {
     NAME(
         path = "name",
         type = String::class.java,
-        allowedOperations = setOf(OperationType.EQUALS, OperationType.LIKE)
     ),
     PRODUCT_NUMBER(
         path = "productNumber",
         type = Long::class.java,
-        allowedOperations = setOf(OperationType.EQUALS, OperationType.GTE, OperationType.LTE)
     ),
     PRICE(
         path = "price",
         type = BigDecimal::class.java,
-        allowedOperations = setOf(OperationType.EQUALS, OperationType.GTE, OperationType.LTE)
     ),
     QUANTITY(
         path = "quantity",
         type = BigDecimal::class.java,
-        allowedOperations = setOf(OperationType.EQUALS, OperationType.GTE, OperationType.LTE)
+    ),
+    CREATED_AT(
+        path = "createdDate",
+        type = LocalDate::class.java,
+    ),
+    QUANTITY_CHANGED(
+        path = "quantityChangedDateTime",
+        type = LocalDateTime::class.java,
     ),
     CATEGORY_TYPE(
         path = "categoryType",
         type = Enum::class.java,
-        allowedOperations = setOf(OperationType.EQUALS),
         enumClass = CategoryType::class.java
     );
+
+    companion object {
+        fun from(field: String): ProductField =
+            entries.firstOrNull { it.path == field }
+                ?: throw IllegalArgumentException("Такого поля для поиска нет: $field")
+    }
 }
