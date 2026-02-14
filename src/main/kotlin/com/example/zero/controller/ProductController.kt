cf80c5156184d3a78d0da5c22074ef60120a797d
Silly <104162046+LaMbUsErNaMe@users.noreply.github.com>
2026-02-14 22:18:52 +0400

    fix

--- src/main/kotlin/com/example/zero/controller/ProductController.kt
+++ src/main/kotlin/com/example/zero/controller/ProductController.kt
@@ -1,11 +1,11 @@
 package com.example.zero.controller
 
 import com.example.zero.controller.dto.request.CreateProductRequest
-import com.example.zero.controller.dto.request.patch.PatchProductRequest
+import com.example.zero.controller.dto.product.request.patch.PatchProductRequest
 import com.example.zero.controller.dto.request.search.SearchFilterDto
-import com.example.zero.controller.dto.request.update.UpdateProductRequest
-import com.example.zero.controller.dto.response.ResponseProduct
-import com.example.zero.services.dto.ProductDto
+import com.example.zero.controller.dto.product.request.update.UpdateProductRequest
+import com.example.zero.controller.dto.product.response.ResponseProduct
+import com.example.zero.services.dto.product.ProductDto
 import jakarta.validation.Valid
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
