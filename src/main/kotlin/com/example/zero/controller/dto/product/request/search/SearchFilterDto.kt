3e1679405ccc7abf35bbc5d8f0d156b04e2cdaab
Silly <104162046+LaMbUsErNaMe@users.noreply.github.com>
2026-02-17 00:34:54 +0400

    fix

--- src/main/kotlin/com/example/zero/controller/dto/product/request/search/SearchFilterDto.kt
+++ src/main/kotlin/com/example/zero/controller/dto/product/request/search/SearchFilterDto.kt
@@ -1,9 +1,7 @@
 package com.example.zero.controller.dto.product.request.search
 
 import com.example.zero.enums.OperationType
-import jakarta.validation.Valid
 import jakarta.validation.constraints.NotBlank
-import jakarta.validation.constraints.NotNull
 data class SearchFilterDto(
     @field:NotBlank
     val field: String,
